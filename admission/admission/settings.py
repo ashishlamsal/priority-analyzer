1f9ade25bceb98c6c20000382e6eddb6dba6996f
ashish-lamsal <ashishlamsal4283@gmail.com>
2021-07-06 19:17:27 +0545

    Fixed Merge Confilct

--- admission/admission/settings.py
+++ admission/admission/settings.py
@@ -77,10 +77,20 @@ WSGI_APPLICATION = 'admission.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'postgres',
+        'USER': 'postgres',
+        'PASSWORD': 'password123',
+        'HOST': 'localhost'
     }
 }
 
